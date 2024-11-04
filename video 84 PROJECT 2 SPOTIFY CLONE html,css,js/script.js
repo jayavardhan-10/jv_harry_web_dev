61d27cc815add5764f1e14fac5688b07b801ca6f
jayavardhan-10 <jayavardhanboguda@gmail.com>
2024-11-05 01:48:26 +0530

    SPOTIFY till 4:02:07 / 5:35:38

--- video 84 PROJECT 2 SPOTIFY CLONE html,css,js/script.js
+++ video 84 PROJECT 2 SPOTIFY CLONE html,css,js/script.js
@@ -1,10 +1,11 @@
 console.log("lets write java scripr");
 // keeping this current song global to assuer that only 1 plays at a time
 let currentSong = new Audio();
+let songs;
 
 function secondsToMinutesSeconds(seconds) {
     if (isNaN(seconds) || seconds < 0) {
-        return "Invalid input";
+        return "00:00";
     }
     
     const minutes = Math.floor(seconds / 60);
@@ -28,7 +29,7 @@ async function getSongs()
     div.innerHTML = response;
     let as = div.getElementsByTagName("a")
 
-    let songs = []
+    songs = []
     for (let index = 0; index < as.length; index++) {
         const element = as[index];
         
@@ -119,7 +120,7 @@ async function main(){
     // listen for time update event
     currentSong.addEventListener("timeupdate", ()=>{
         // console.log(currentSong.currentTime, currentSong.duration);
-        document.querySelector(".songtime").innerHTML = `${secondsToMinutesSeconds(currentSong.currentTime)}/${secondsToMinutesSeconds(currentSong.duration)}`
+        document.querySelector(".songtime").innerHTML = `${secondsToMinutesSeconds(currentSong.currentTime)} / ${secondsToMinutesSeconds(currentSong.duration)}`
 
         // to move the circle on the line when the song plays and timechanges or updates
         document.querySelector(".circle").style.left = currentSong.currentTime/currentSong.duration * 100 + "%"
@@ -141,7 +142,50 @@ async function main(){
     })
 
 
+    // add and event listener for hamburger
+    document.querySelector(".hamburger").addEventListener("click", ()=>{
+        document.querySelector(".left").style.left = "0";
+    })
+    
+    // add and event listener for close button
+    document.querySelector(".close").addEventListener("click", ()=>{
+        document.querySelector(".left").style.left = "-125%";   
+    })
 
+    // add event listeners for previous
+    previous.addEventListener("click", ()=>{
+        currentSong.pause();
+        console.log("previous clicked");
+        let index = songs.indexOf(currentSong.src.split("/").slice(-1) [0])
+        if(index-1 >= 0){
+
+            // to play the previouz song when tapped
+            playMusic(songs[index-1]);
+        }
+    })
+    
+    // add event listeners for next
+    next.addEventListener("click", ()=>{
+        currentSong.pause();
+        console.log("next clicked");
+
+
+        // knowing the index of the present song
+        let index = songs.indexOf(currentSong.src.split("/").slice(-1) [0])
+        
+        if(index+1 < songs.length){
+
+            // to play the next song when tapped
+            playMusic(songs[index+1]);
+        }
+    })
+
+    // Add an event to volume
+    document.querySelector(".range").getElementsByTagName("input")[0].addEventListener("change", (e)=>{
+        console.log("setting volume to ", e.target.value);
+        currentSong.volume = parseInt(e.target.value)/100;
+    })
+    
 }
 
 
