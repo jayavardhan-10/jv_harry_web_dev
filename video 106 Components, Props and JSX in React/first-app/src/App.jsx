e442eb4f548301b6f0c284c0159b27cc16169e08
jayavardhan-10 <jayavardhanboguda@gmail.com>
2024-11-17 07:16:45 +0530

    video 106 done components props and jsx in react

--- video 106 Components, Props and JSX in React/first-app/src/App.jsx
+++ video 106 Components, Props and JSX in React/first-app/src/App.jsx
@@ -11,10 +11,10 @@ function App() {
       <Navbar/>
       
       <div className="cards">
-        <Card title="card 1" />
-        <Card/>
-        <Card/>
-        <Card/>
+        <Card title="card 1" description="card 1 desc" />
+        <Card title="card 2" description="card 2 desc" />
+        <Card title="card 3" description="card 3 desc" />
+        <Card title="card 4" description="card 4 desc" />
       </div>
       <Footer/>
     </>
--- video 106 Components, Props and JSX in React/first-app/src/components/Card.jsx
+++ video 106 Components, Props and JSX in React/first-app/src/components/Card.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import "./Card.css"
-const Card = () => {
+const Card = (props) => {
   return (
-    <div className='card'>
-        <h1>Title of the Card</h1>
-        <p>Description of the Card</p>
+    <div className='card' style={{backgroundColor:"gray", overflow:"hidden", border:"1px solid black"}}>
+        <img src="https://plus.unsplash.com/premium_photo-1663100722417-6e36673fe0ed?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Y29kaW5nfGVufDB8fDB8fHww" alt="" width={270} />
+        <h1>{props.title}</h1>
+        <p>{props.description}</p>
     </div>
   )
 }
