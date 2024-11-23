158a714dfa575bfc1a531328e0530ed155e2f716
jayavardhan-10 <jayavardhanboguda@gmail.com>
2024-11-23 19:01:31 +0530

    video 114 DONE TodoList App using React, Tailwind & React Icons

--- video 114 TodoList App using React, Tailwind & React Icons/src/components/Navbar.jsx
+++ video 114 TodoList App using React, Tailwind & React Icons/src/components/Navbar.jsx
@@ -2,11 +2,14 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav className='flex justify-between bg-slate-500'>
-        <ul className="flex">
-            <li>Home</li>
-            <li>About</li>
-            <li>Contact Us</li>
+    <nav className='flex justify-between bg-purple-900 text-white py-2'>
+      <div className="logo">
+        <span className='font-bold text-xl mx-8'>Tasker</span>
+      </div>
+        <ul className="flex gap-8 mx-9">
+            <li className='cursor-pointer hover:font-bold transition-all'>Home</li>
+            <li className='cursor-pointer hover:font-bold transition-all'>About</li>
+            <li className='cursor-pointer hover:font-bold transition-all'>Contact Us</li>
         </ul>
     </nav>
   )
