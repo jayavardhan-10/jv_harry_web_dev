a9f1e1239fbe422b5345872e1065fb11b3d81509
jayavardhan-10 <jayavardhanboguda@gmail.com>
2024-11-25 21:56:51 +0530

    video 115

--- video 115 React Router Routing in React/src/App.jsx
+++ video 115 React Router Routing in React/src/App.jsx
@@ -22,7 +22,7 @@ function App() {
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
-        </button>
+        </button>                   
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
