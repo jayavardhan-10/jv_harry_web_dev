39dd169fac58c1fcf3641f63402b2bd4b76039dc
jayavardhan-10 <jayavardhanboguda@gmail.com>
2024-11-25 21:56:42 +0530

    video 115

--- video 115 React Router Routing in React/src/App.jsx
+++ video 115 React Router Routing in React/src/App.jsx
@@ -7,7 +7,7 @@ import Navbar from './components/Navbar'
 function App() {
   const [count, setCount] = useState(0)
 
-  return (
+  return (                             
     <>
       <div>
         <Navbar/>
