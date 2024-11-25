78d3087f5bef15ef11c8a1fd30d13320b13a3bd7
jayavardhan-10 <jayavardhanboguda@gmail.com>
2024-11-25 21:58:27 +0530

    video 115

--- video 115 React Router Routing in React/src/App.jsx
+++ video 115 React Router Routing in React/src/App.jsx
@@ -29,7 +29,10 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>              
+      </p>  
+
+      
+                  
     </>
   )
 }
